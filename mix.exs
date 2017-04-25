b24a83d0e16e4bf8894fa7324cd1942e7ca0ab7c
Philipp <git@pentacent.com>
2017-04-25 10:44:04 +0200

    Bump version to v0.1.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Belt.Mixfile do
 
   def project do
     [app: :belt,
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
